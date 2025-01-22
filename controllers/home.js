aa49ced1b1d9e1d66522a498ac0dce1f3f5dfbf2
unknown <nateware777@gmail.com>
2025-01-22 08:23:55 +0300

    massive changes

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,19 @@
+const FeedItem = require("../models/FeedItem"); // Ensure you import the model
+
 module.exports = {
-  getIndex: (req, res) => {
-    res.render("index.ejs");
+  getIndex: async (req, res) => {
+    try {
+      const feedItems = await FeedItem.find()
+        .sort({ price: -1 }) // Sort by price descending
+        .lean(); // Convert to plain JavaScript objects
+
+      res.render("index", {
+        feedItems: feedItems,
+        user: req.isAuthenticated() ? req.user : null,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Server Error");
+    }
   },
 };
